dbc963def4a2216237872107ff66dd3b00b13c34
490shigure <kotlyne@outlook.com>
2025-03-31 18:58:22 +0800

    refactor: restructure Index.vue with new components for improved organization
    - Replaced inline template elements in Index.vue with separate IndexHeader and IndexBody components for better modularity.
    - Removed unused greeting functionality and command line argument display from Index.vue.
    - Updated localization files to include new keys for header elements in both English and Chinese.

---
+++ src/components/IndexBody.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import { localeMap } from '@/i18n';
+</script>
+
+<template>
+    <div class="flex flex-col h-full">
+        <select id="locale" v-model="$i18n.locale" class="w-fit border-1 border-gray-300 rounded-md p-2 m-2">
+            <option v-for="locale in $i18n.availableLocales" :key="locale" :value="locale">
+                {{ localeMap[locale as keyof typeof localeMap] }}</option>
+        </select>
+    </div>
+</template>
