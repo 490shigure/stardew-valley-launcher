c2957916e930fef155498ca56df604f486652de2
490shigure <kotlyne@outlook.com>
2025-05-23 01:00:45 +0800

    优化组件布局和样式
    - 在Index.vue中为动态组件添加最小高度，确保在不同内容情况下的布局稳定性。
    - 在ModManage.vue中调整组件结构，确保表格模块和工具栏的高度适应性，提升用户体验。
    - 在ModTable.vue中优化表格容器的高度设置，确保在内容溢出时的可读性和交互性。

--- src/index.vue
+++ src/index.vue
@@ -32,7 +32,7 @@ onMounted(async () => {
         <IndexHeader :active-manage="activeManage" @toggle-manage-view="toggleManageView" />
         <!-- 主内容区域 -->
         <keep-alive>
-            <component :is="activeManage === 'mods' ? ModManage : SavesManage" class="flex-1" />
+            <component :is="activeManage === 'mods' ? ModManage : SavesManage" class="flex-1 min-h-0" />
         </keep-alive>
         <!-- 额外区域 Section3 -->
         <!-- <div class="bg-purple-500 text-white p-4">
